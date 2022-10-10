747f04dfa6db425031b547ff75a3425f2714a0aa
Victor Maia <74581521+victormaiaaa@users.noreply.github.com>
2022-10-10 00:56:07 -0300

    Update JS

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
-const selecione = document.querySelector(".select");
-const incremento = document.querySelector(".chave-container");
-const btn = document.querySelector("button");
-const radiobtn = document.querySelector(".radio-button");
-const codificar = document.querySelector("#codificar");
-const decodificar = document.querySelector("#decodificar");
+var btnCrypt = document.querySelector("#btn-crypt");
+var btnDecrypt = document.querySelector("#btn-decrypt");
+var radiobtn = document.querySelector(".radio-button");
+var codificar = document.querySelector("#codificar");
+var decodificar = document.querySelector("#decodificar");
 
-selecione.addEventListener("click", function () {
-  if (selecione.value == "cifra") {
-    incremento.style.display = "block";
-  } else {
-    incremento.style.display = "none";
-  }
-});
-
-function cifraCesar() {
+function crypt() {
   let msg = document.querySelector("#mensagem").value;
   let chave = parseInt(document.querySelector("#rangenumber").value);
-  let saida = '';
-
-  if (codificar.checked) {
-    for (let i = 0; i < msg.length; i++) {
-      if (msg[i] === msg[i].toUpperCase()) {
-        saida += String.fromCharCode((msg.charCodeAt(i) + chave - 65) % 26 + 65); 
-      } else {
-        saida += String.fromCharCode((msg.charCodeAt(i) + chave - 97) % 26 + 97);
-      }
+  let saida = "";
+  for (let i = 0; i < msg.length; i++) {
+    if (msg[i] === msg[i].toUpperCase()) {
+      saida += String.fromCharCode(
+        ((msg.charCodeAt(i) + chave - 65) % 26) + 65
+      );
+    } else {
+      saida += String.fromCharCode(
+        ((msg.charCodeAt(i) + chave - 97) % 26) + 97
+      );
     }
-    return saida;
-
-  } else if (decodificar.checked) {
-    for (let i = 0; i < msg.length; i++) {
-      if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
-        saida += String.fromCharCode((msg.charCodeAt(i) - 97 -  chave + 26) % 26 + 97);
-      } else if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
-        saida += String.fromCharCode((msg.charCodeAt(i) - 65 - chave + 26) % 26 + 65);
-      } else {
-        saida += String.fromCharCode(msg.charCodeAt(i));
-      }
-    }
-    return saida;
   }
+  return saida;
 }
 
-radiobtn.addEventListener("click", function () {
-  if (codificar.checked) {
-    btn.innerHTML = "Codificar Mensagem!";
-  } else if (decodificar.checked) {
-    btn.innerHTML = "Decodificar Mensagem!";
+function decrypt() {
+  let msg = document.querySelector("#mensagem").value;
+  let chave = parseInt(document.querySelector("#rangenumber").value);
+  let saida = "";
+  for (let i = 0; i < msg.length; i++) {
+    if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
+      saida += String.fromCharCode(
+        ((msg.charCodeAt(i) - 97 - chave + 26) % 26) + 97
+      );
+    } else if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
+      saida += String.fromCharCode(
+        ((msg.charCodeAt(i) - 65 - chave + 26) % 26) + 65
+      );
+    } else {
+      saida += String.fromCharCode(msg.charCodeAt(i));
+    }
   }
-  return resultado.innerText = cifraCesar();
+  return saida;
+}
+
+btnCrypt.addEventListener("click", function (event) {
+  event.preventDefault();
+  resultado.innerText = crypt();
 });
 
-btn.addEventListener("click", function (event) {
+btnDecrypt.addEventListener("click", function (event) {
   event.preventDefault();
-  if (selecione.value === "cifra") {
-    resultado.innerText = cifraCesar();
-  }
+  resultado.innerText = decrypt();
 });
